1a63dcb9a1c6129411fc4c67a43f910b331cf173
syh03 <1336840590@qq.com>
2018-01-04 23:55:12 +0800

    commit

--- assignment1/test/SVM.py
+++ assignment1/test/SVM.py
@@ -4,15 +4,16 @@ Created on 2018/1/4
 
 @author: sunyihuan
 '''
-import random
+import math
 import numpy as np
-from assignment1.data_utils import load_CIFAR10
-import matplotlib.pyplot as plt
-from assignment1.classifiers.linear_svm import svm_loss_naive
 import time
 import scipy.io as scio
-from assignment1.gradient_check import grad_check_sparse
+import matplotlib.pyplot as plt
 
+from assignment1.data_utils import load_CIFAR10
+from assignment1.classifiers.linear_svm import svm_loss_naive, svm_loss_vectorized
+from assignment1.classifiers import LinearSVM
+from assignment1.gradient_check import grad_check_sparse
 
 cifar10_dir = '../datasets/cifar-10-batches-py'
 X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
@@ -65,7 +66,6 @@ X_dev = np.hstack([X_dev, np.ones((X_dev.shape[0], 1))])
 loss, grad = svm_loss_naive(W, X_dev, y_dev, 0.000005)
 print('loss: %f' % (loss,))
 
-
 loss, grad = svm_loss_naive(W, X_dev, y_dev, 0.0)
 
 # Numerically compute the gradient along several randomly chosen dimensions, and
@@ -79,3 +79,121 @@ grad_numerical = grad_check_sparse(f, W, grad)
 loss, grad = svm_loss_naive(W, X_dev, y_dev, 5e1)
 f = lambda w: svm_loss_naive(w, X_dev, y_dev, 5e1)[0]
 grad_numerical = grad_check_sparse(f, W, grad)
+
+tic = time.time()
+loss_naive, grad_naive = svm_loss_naive(W, X_dev, y_dev, 0.000005)
+toc = time.time()
+print('Naive loss: %e computed in %fs' % (loss_naive, toc - tic))
+
+tic = time.time()
+loss_vectorized, _ = svm_loss_vectorized(W, X_dev, y_dev, 0.000005)
+toc = time.time()
+print('Vectorized loss: %e computed in %fs' % (loss_vectorized, toc - tic))
+
+# The losses should match but your vectorized implementation should be much faster.
+print('difference: %f' % (loss_naive - loss_vectorized))
+
+tic = time.time()
+_, grad_naive = svm_loss_naive(W, X_dev, y_dev, 0.000005)
+toc = time.time()
+print('Naive loss and gradient: computed in %fs' % (toc - tic))
+
+tic = time.time()
+_, grad_vectorized = svm_loss_vectorized(W, X_dev, y_dev, 0.000005)
+toc = time.time()
+print('Vectorized loss and gradient: computed in %fs' % (toc - tic))
+
+# The loss is a single number, so it is easy to compare the values computed
+# by the two implementations. The gradient on the other hand is a matrix, so
+# we use the Frobenius norm to compare them.
+difference = np.linalg.norm(grad_naive - grad_vectorized, ord='fro')
+print('difference: %f' % difference)
+
+svm = LinearSVM()
+tic = time.time()
+loss_hist = svm.train(X_train, y_train, learning_rate=1e-7, reg=2.5e4,
+                      num_iters=1500, verbose=True)
+toc = time.time()
+print('That took %fs' % (toc - tic))
+
+plt.plot(loss_hist)
+plt.xlabel('Iteration number')
+plt.ylabel('Loss value')
+plt.show()
+
+y_train_pred = svm.predict(X_train)
+print('training accuracy: %f' % (np.mean(y_train == y_train_pred),))
+y_val_pred = svm.predict(X_val)
+print('validation accuracy: %f' % (np.mean(y_val == y_val_pred),))
+
+learning_rates = [1.4e-7, 1.5e-7, 1.6e-7]
+regularization_strengths = [3e4, 3.1e4, 3.2e4, 3.3e4, 3.4e4]
+
+# results is dictionary mapping tuples of the form
+# (learning_rate, regularization_strength) to tuples of the form
+# (training_accuracy, validation_accuracy). The accuracy is simply the fraction
+# of data points that are correctly classified.
+results = {}
+best_val = -1  # The highest validation accuracy that we have seen so far.
+
+params = [(x, y) for x in learning_rates for y in regularization_strengths]
+for lrate, regular in params:
+    svm = LinearSVM()
+    loss_hist = svm.train(X_train, y_train, learning_rate=lrate, reg=regular,
+                          num_iters=700, verbose=False)
+    y_train_pred = svm.predict(X_train)
+    accuracy_train = np.mean(y_train == y_train_pred)
+    y_val_pred = svm.predict(X_val)
+    accuracy_val = np.mean(y_val == y_val_pred)
+    results[(lrate, regular)] = (accuracy_train, accuracy_val)
+    if (best_val < accuracy_val):
+        best_val = accuracy_val
+        best_svm = svm
+
+# Print out results.
+for lr, reg in sorted(results):
+    train_accuracy, val_accuracy = results[(lr, reg)]
+    print('lr %e reg %e train accuracy: %f val accuracy: %f' % (
+        lr, reg, train_accuracy, val_accuracy))
+
+print('best validation accuracy achieved during cross-validation: %f' % best_val)
+
+x_scatter = [math.log10(x[0]) for x in results]
+y_scatter = [math.log10(x[1]) for x in results]
+
+# plot training accuracy
+marker_size = 100
+colors = [results[x][0] for x in results]
+plt.subplot(2, 1, 1)
+plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+plt.colorbar()
+plt.xlabel('log learning rate')
+plt.ylabel('log regularization strength')
+plt.title('CIFAR-10 training accuracy')
+
+# plot validation accuracy
+colors = [results[x][1] for x in results]  # default size of markers is 20
+plt.subplot(2, 1, 2)
+plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+plt.colorbar()
+plt.xlabel('log learning rate')
+plt.ylabel('log regularization strength')
+plt.title('CIFAR-10 validation accuracy')
+plt.show()
+
+y_test_pred = best_svm.predict(X_test)
+test_accuracy = np.mean(y_test == y_test_pred)
+print('linear SVM on raw pixels final test set accuracy: %f' % test_accuracy)
+
+w = best_svm.W[:-1, :]  # strip out the bias
+w = w.reshape(32, 32, 3, 10)
+w_min, w_max = np.min(w), np.max(w)
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+for i in range(10):
+    plt.subplot(2, 5, i + 1)
+
+    # Rescale the weights to be between 0 and 255
+    wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+    plt.imshow(wimg.astype('uint8'))
+    plt.axis('off')
+    plt.title(classes[i])
